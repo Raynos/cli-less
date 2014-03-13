bfb099c1f6653057f8f00e2cb91fb99424768204
Raynos <raynos2@gmail.com>
2014-03-13 01:46:02 -0700

    print footer at right location

--- render.js
+++ render.js
@@ -3,6 +3,8 @@ module.exports = Render
 function Render(charm, state) {
     var buffer = state.helpMode ?
         state.helpBuffer : state.mainBuffer
+    var footerLoc = buffer.lines.length < state.height ?
+        buffer.lines.length : state.height
 
     charm.push()
     charm.cursor(false)
@@ -17,11 +19,16 @@ function Render(charm, state) {
     charm.push()
     charm.position(1, state.height)
     charm.erase('end')
+    charm.pop()
+
+    charm.push()
+    charm.position(1, footerLoc)
+    charm.erase('end')
     charm.foreground('black')
     charm.background('white')
     charm.write(buffer.footer)
 
     charm.pop()
-    charm.position(buffer.footer.length + 1, state.height)
+    charm.position(buffer.footer.length + 1, footerLoc)
     charm.display('reset')
 }
