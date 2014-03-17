75665ffb9eab8e4b575d5fcfd06862fd1fd48436
Raynos <raynos2@gmail.com>
2014-03-17 01:53:45 -0700

    remove procedural code in comments

--- render.js
+++ render.js
@@ -18,40 +18,6 @@ function Render(state) {
             y: footerLoc
         })
     ])
-
-    /*
-    charm.push()
-    charm.cursor(false)
-    for (var i = 0; i < state.height; i++) {
-        charm.position(1, i)
-        charm.erase('end')
-        charm.write(buffer.lines[buffer.index + i - 1] || '')
-    }
-    charm.cursor(true)
-    charm.pop()
-    */
-
-    /*
-    charm.push()
-    charm.position(1, state.height)
-    charm.erase('end')
-    charm.pop()
-    */
-
-    /*
-    charm.push()
-    charm.position(1, footerLoc)
-    charm.erase('end')
-    charm.foreground('black')
-    charm.background('white')
-    charm.write(buffer.footer)
-    */
-
-    /*
-    charm.pop()
-    charm.position(buffer.footer.length + 1, footerLoc)
-    charm.display('reset')
-    */
 }
 
 function footer(buffer, footerLoc) {
